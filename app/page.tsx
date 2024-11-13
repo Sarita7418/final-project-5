04ff22fcc121041c3d0fe6e265f7d2a53e3b9ea6
wararocio <huanapacowara@gmail.com>
2024-11-13 12:34:12 -0400

    conponentes config

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Button variant="outline" className="rojo">
-        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/configuracion">Configuración</Link>
       </Button>
     </div>
   );
