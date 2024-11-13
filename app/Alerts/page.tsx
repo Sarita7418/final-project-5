d78a7fc41516d109bb3b3c0f7e91d8f666940cea
evelynburgoa <evelynburgoa04@gmail.com>
2024-11-13 12:20:04 -0400

    paneles

---
+++ app/Alerts/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import Menu from './components/Menu'
+
+function alerts() {
+  return (
+    <div>
+      <Header />
+      <section className="content">
+        <Menu />
+        <section className="dashboard_content">
+          
+        </section>
+      </section>
+</div>
+  )
+}
+
+export default alerts
