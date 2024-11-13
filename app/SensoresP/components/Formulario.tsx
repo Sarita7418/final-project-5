36b08408530b7b0ef02d1c96f2d1cefab26ca60f
Cristhian Rojas <cristianrojascussi@gmail.com>
2024-11-13 12:19:59 -0400

    panel

---
+++ app/SensoresP/components/Formulario.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+const Formulario = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Formulario
--- app/SensoresP/components/Menu.tsx
+++ app/SensoresP/components/Menu.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Link from "next/link";
-import "./Menu.css";
-import dashboard from "@/public/home 2.svg";
+import "@/components/Menu.css";
+import dashboard from "@/public/home 2 plomo.svg";
 import lectura from "@/public/carbon_report.svg";
-import sensor from "@/public/material-symbols_sensors-krx-outline-rounded.svg";
+import sensor from "@/public/sensormorado.svg";
 import alerta from "@/public/service 1.svg";
 import config from "@/public/settings solid 1.svg";
 import area from "@/public/mdi_report-line.svg";
@@ -12,12 +12,12 @@ const Menu = () => {
   return (
     <nav className="sidebar">
       <ul className="menu">
-        <li className="menu_element selected_menu">
-          <div className="rectangle_menu"></div>
+        <li className="menu_element ">
           <img src={dashboard.src} alt="" />
           <Link href="/dashboard">Dashboard</Link>
         </li>
-        <li className="menu_element">
+        <li className="menu_element selected_menu">
+          <div className="rectangle_menu"></div>
           <img src={sensor.src} alt="" />
           <Link href="/SensoresP">Sensores</Link>
         </li>
