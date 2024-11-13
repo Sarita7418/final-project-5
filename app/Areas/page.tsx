5d731fe48b92c3a5885b0f2ba4f373ea8c17e800
Brayancrack133 <brayanjj.ch@gmail.com>
2024-11-13 12:34:13 -0400

    panel de control

---
+++ app/Areas/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+const Areas = () => {
+  return (
+    <div>
+      <Header />
+      <section className="content">
+        <Menu />
+        <section>
+         
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default Areas;
