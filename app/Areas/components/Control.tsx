64a5bfdb3e73877f3388519bf9d1f0382579be02
Brayancrack133 <brayanjj.ch@gmail.com>
2024-11-13 21:33:35 -0400

    component Control finish

--- app/Areas/components/Control.tsx
+++ app/Areas/components/Control.tsx
@@ -1,13 +1,24 @@
 // components/FloorControl.js
 import React from "react";
+import Link from "next/link";
+import Plus from "@/public/plus.svg";
+import Calendar from "@/public/calendar.svg";
 import "./Control.css";
 
 const FloorControl = () => {
   return (
-    <div className="floor-control">
-      <h3>Control De Pisos</h3>
-      <button className="add-area-btn">Añadir Áreas</button>
-      <button className="all-areas-btn">Todas Las Áreas</button>
+    <section className="floor-control">
+      <div className="options">
+        <h3>Control De Pisos</h3>
+        <button className="add-area-btn">
+          <img src={Plus.src} alt="" />
+          <Link href="">Añadir Áreas</Link>
+        </button>
+        <button className="all-areas-btn">
+          <img src={Calendar.src} alt="" />
+          <Link href="">Todas Las Áreas</Link>
+        </button>
+      </div>
       <div className="floor-selector">
         <label htmlFor="floor-select">Seleccionar Piso:</label>
         <select id="floor-select">
@@ -16,7 +27,7 @@ const FloorControl = () => {
           <option>Tercer Piso</option>
         </select>
       </div>
-    </div>
+    </section>
   );
 };
 
