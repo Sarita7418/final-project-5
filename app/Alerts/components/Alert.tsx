bd2b1717f503121f258f7def8812c5387a14eb66
evelynburgoa <evelynburgoa04@gmail.com>
2024-11-13 21:25:13 -0400

    Notificacion de alertas, anomalias y consumos

--- app/Alerts/components/Alert.tsx
+++ app/Alerts/components/Alert.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import './Alert.css';
 import Icon from './Icon';
-import Button from './Button';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const Alert = () => {
-  return (
-    <div className="alert-container">
-      <Icon/>
-      <span className="alert-text">Alerta de Límite de Consumo</span>
-      <span></span>
-      <Button label="Eliminar" disabled />
-      <Button label="Verificar" />
-    </div>
-  );
-};
-
+const Alert = () => { 
+  return ( 
+  <div className="alerts"> 
+    <section className="alert-container"> 
+      <Icon type="alert"/> 
+      <div className="alert-content"> 
+        <span className="alert-text">Alerta Detectada</span> 
+        <div className="alert-buttons">
+        <Button variant="outline" className="buttondisabled">
+            <Link href="">Eliminar</Link>
+          </Button>
+          <Button variant="outline" className="button">
+            <Link href="/dashboard">Verificar</Link>
+          </Button>
+        </div> 
+      </div> 
+    </section> 
+    <section className="alert-container"> 
+      <Icon type="anomaly"/> 
+      <div className="alert-content"> 
+        <span className="alert-text">Anomalía Detectada</span> 
+        <div className="alert-buttons">
+        <Button variant="outline" className="buttondisabled">
+            <Link href="">Eliminar</Link>
+          </Button>
+          <Button variant="outline" className="button">
+            <Link href="/dashboard">Verificar</Link>
+          </Button>
+        </div>
+      </div> 
+    </section> 
+    <section className="alert-container"> 
+      <Icon type="consumption"/> 
+      <div className="alert-content"> 
+        <span className="alert-text">Consumo Estabilizado</span> 
+        <div className="alert-buttons">
+        <Button variant="outline" className="buttondisabled">
+            <Link href="">Eliminar</Link>
+          </Button>
+          <Button variant="outline" className="button">
+            <Link href="/dashboard">Verificar</Link>
+          </Button>
+        </div>
+      </div> 
+    </section> 
+  </div> 
+  ); 
+}; 
 export default Alert;
--- app/Alerts/page.tsx
+++ app/Alerts/page.tsx
@@ -10,7 +10,7 @@ function alerts() {
       <Header />
       <section className="content">
         <Menu />
-        <section className="alerts_content">
+        <section className="page_content">
           <Alert/>
         </section>
       </section>
