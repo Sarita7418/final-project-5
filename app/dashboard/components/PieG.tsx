1a12e78ae6d7a1009ca342592f1da19ac5b6ffb4
Sarita731 <mg6667418@gmail.com>
2024-11-14 21:28:50 -0400

    xd

--- app/dashboard/components/PieG.tsx
+++ app/dashboard/components/PieG.tsx
@@ -78,7 +78,7 @@ export function PieG() {
   const months = React.useMemo(() => desktopData.map((item) => item.month), [])
 
   return (
-    <Card data-chart={id} className="flex flex-col">
+    <Card data-chart={id} className="flex flex-col pie_card">
       <ChartStyle id={id} config={chartConfig} />
       <CardHeader className="flex-row items-start space-y-0 pb-0">
         <div className="grid gap-1">
