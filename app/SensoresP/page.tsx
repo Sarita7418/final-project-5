a6f9d4a87b0a11598d0fdc53c66209ce112b8bf3
Cristhian Rojas <cristianrojascussi@gmail.com>
2024-11-15 19:35:41 -0400

    Correccion de componentes

--- app/SensoresP/page.tsx
+++ app/SensoresP/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
-import Listasensor from "./components/Listasensor";
+import Listasensor from "../../components/Listasensor";
 
 const sensor = () => {
   return (
--- components/Formulario.tsx
+++ components/Formulario.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "@/app/SensoresP/components/Formulario.css";
+import "@/components/Formulario.css";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
