2d0a8c8af245c92635537a7a012269c7bc733a05
Sarita731 <mg6667418@gmail.com>
2024-11-17 19:32:33 -0400

    dash100

--- components/ConsumoGC.tsx
+++ components/ConsumoGC.tsx
@@ -1,21 +1,67 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import { ComparativeG } from "./ComparativeG";
 import { PieG } from "./PieG";
-import "./ConsumoGC.css"
+import "./ConsumoGC.css";
 
 const ConsumoGC = () => {
+  const [selectedTab, setSelectedTab] = useState<string>("agua");
+
+  const handleTabChange = (tab: string) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <section className="container_consumo">
-     <span className="span-title">
-        Consumo Semanal
-     </span>
-     <div className="graficos">
-      <ComparativeG />
-      <PieG />  
-     </div>
-      
+      <span className="span-title">Consumo Semanal</span>
+
+      {/* Menú de pestañas */}
+      <div className="tabs">
+        <button
+          className={`tab ${selectedTab === "agua" ? "active_tab" : ""}`}
+          onClick={() => handleTabChange("agua")}
+        >
+          Agua
+        </button>
+        <button
+          className={`tab ${selectedTab === "electricidad" ? "active_tab" : ""}`}
+          onClick={() => handleTabChange("electricidad")}
+        >
+          Electricidad
+        </button>
+        <button
+          className={`tab ${selectedTab === "gas" ? "active_tab" : ""}`}
+          onClick={() => handleTabChange("gas")}
+        >
+          Gas
+        </button>
+      </div>
+
+      {/* Contenido dinámico basado en la pestaña seleccionada */}
+      <div className="graficos">
+        {selectedTab === "agua" && (
+          <>
+            <ComparativeG />
+            <PieG />
+          </>
+        )}
+        {selectedTab === "electricidad" && (
+          <>
+            <ComparativeG />
+            <PieG />
+          </>
+        )}
+        {selectedTab === "gas" && (
+          <>
+            <ComparativeG />
+            <PieG />
+          </>
+        )}
+      </div>
     </section>
   );
 };
 
 export default ConsumoGC;
+
