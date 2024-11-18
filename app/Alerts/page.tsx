e4ce3d6f815e4d4876016473267dac06c79b22c2
Sarita731 <mg6667418@gmail.com>
2024-11-17 21:46:21 -0400

    finished

--- app/Alerts/page.tsx
+++ app/Alerts/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '@/components/Header'
 import Menu from '@/components/Menu'
-import Alert from './components/Alert'
+import Alert from '@/components/Alert'
 
 
 function alerts() {
